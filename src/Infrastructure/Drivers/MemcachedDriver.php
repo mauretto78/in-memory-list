4290672036aa80a4a402ed0de97bda82f6406bab
Mauro <m.cassani@bestnetwork.it>
2017-05-18 15:10:34 +0200

    drivers folder

---
+++ src/Infrastructure/Drivers/MemcachedDriver.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the InMemoryList package.
+ *
+ * (c) Mauro Cassani<https://github.com/mauretto78>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace InMemoryList\Infrastructure\Drivers;
+
+use InMemoryList\Infrastructure\Drivers\Contracts\DriverInterface;
+
+class MemcachedDriver implements DriverInterface
+{
+
+}
